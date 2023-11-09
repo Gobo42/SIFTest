194cdfef7e26c73d7bd83029459ae1691affbc94
Gobo42 <hum.matt@gmail.com>
2023-11-08 16:21:01 -0800

    Created Windows Client with UI

---
+++ WinClient/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace SIFTestWinClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            textBox4 = new TextBox();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(164, 88);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(240, 43);
+            textBox1.TabIndex = 0;
+            textBox1.KeyDown += textBox1_KeyDown;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(496, 85);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(95, 43);
+            textBox2.TabIndex = 1;
+            textBox2.Text = "8080";
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 91);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 37);
+            label1.TabIndex = 2;
+            label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(425, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 37);
+            label2.TabIndex = 3;
+            label2.Text = "Port";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(634, 80);
+            button1.Name = "button1";
+            button1.Size = new Size(186, 51);
+            button1.TabIndex = 4;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(24, 357);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.ReadOnly = true;
+            textBox3.Size = new Size(1035, 372);
+            textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.Location = new Point(844, 77);
+            button2.Name = "button2";
+            button2.Size = new Size(186, 51);
+            button2.TabIndex = 6;
+            button2.Text = "Disconnect";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Enabled = false;
+            button3.Location = new Point(565, 176);
+            button3.Name = "button3";
+            button3.Size = new Size(231, 54);
+            button3.TabIndex = 7;
+            button3.Text = "Send Command";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Enabled = false;
+            textBox4.Location = new Point(62, 182);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(497, 43);
+            textBox4.TabIndex = 8;
+            textBox4.KeyDown += textBox4_KeyDown;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(62, 251);
+            label3.Name = "label3";
+            label3.Size = new Size(534, 74);
+            label3.TabIndex = 9;
+            label3.Text = "use the command \"connect <port>\"\r\n<port> is the port that you want to listen to";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1094, 760);
+            Controls.Add(label3);
+            Controls.Add(textBox4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "SIF Test Client";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox3;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox4;
+        private Label label3;
+    }
+}
---
+++ WinClient/Form1.cs
@@ -0,0 +1,185 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SIFTestWinClient
+{
+    public partial class Form1 : Form
+    {
+        private bool Connected;
+        private Socket client { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var ServerAddr = string.Empty;
+            var ServerPort = string.Empty;
+
+            ServerAddr = textBox1.Text.Trim();
+            ServerPort = textBox2.Text.Trim();
+            textBox3.AppendText("Connecting to " + ServerAddr + ":" + ServerPort + "\r\n");
+            button1.Enabled = false;
+
+            try
+            {
+                IPHostEntry ipHostInfo = await Dns.GetHostEntryAsync(ServerAddr);
+                IPAddress rServerIP = ipHostInfo.AddressList[0];
+                IPEndPoint ServerIP = new(rServerIP, Int32.Parse(ServerPort));
+
+                client = new Socket(ServerIP.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                client.Connect(ServerIP);
+                byte[] messageBytes = Encoding.ASCII.GetBytes("Hi\n");
+                _ = client.Send(messageBytes);
+
+                byte[] buffer = new byte[1024];
+                int byterecv = client.Receive(buffer);
+                var resp = Encoding.ASCII.GetString(buffer, 0, byterecv);
+
+                if (resp.StartsWith("Hello"))
+                {
+                    Connected = true;
+                    button2.Enabled = true;
+                    textBox3.AppendText("Control channel Established.\r\n");
+                    textBox4.Enabled = true;
+                    button3.Enabled = true;
+                    textBox4.Select();
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    textBox3.AppendText("Couldn't connect to " + textBox1.Text.Trim() + "\r\n");
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+            catch (FormatException err)
+            {
+                button1.Enabled = true;
+                textBox3.AppendText("Error: " + err.ToString());
+            }
+            catch (ArgumentNullException err)
+            {
+                button1.Enabled = true;
+                textBox3.AppendText("Argument Null Exception: " + err.ToString());
+            }
+            catch (SocketException err)
+            {
+                button1.Enabled = true;
+                textBox3.AppendText($"Socket Error: {err.Message}");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] messageBytes = Encoding.ASCII.GetBytes("exit\n");
+            _ = client.Send(messageBytes);
+            client.Shutdown(SocketShutdown.Both);
+            client.Close();
+            textBox3.AppendText("Closed Control Channel\r\n");
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            textBox4.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var command = textBox4.Text.Trim();
+            if (command != "")
+            {
+                textBox4.Text = "";
+                if (command.StartsWith("connect"))
+                {
+                    var lport = command.Split(' ')[1];
+                    textBox3.AppendText("Trying to open local port " + lport + "\r\n");
+                    var udpport = Int32.Parse(lport.Trim());
+                    UdpClient udpserver = new UdpClient(udpport);
+                    textBox3.AppendText("Opened data channel on UDP" + lport + "\r\n");
+
+                    byte[] messageBytes = Encoding.ASCII.GetBytes(command + "\n");
+                    _ = client.Send(messageBytes);
+                    textBox3.AppendText("Sending: " + command + "\r\n");
+
+                    while (true)
+                    {
+                        var remote = new IPEndPoint(IPAddress.Any, udpport);
+                        byte[] rdata = udpserver.Receive(ref remote);
+                        string recvd = Encoding.ASCII.GetString(rdata);
+                        textBox3.AppendText("Got: " + recvd + "\r\n");
+                        // udpserver.Send(new byte[] { 1 }, 1, remote);
+                        if (recvd.StartsWith("EXIT"))
+                        {
+                            break;
+                        }
+                    }
+                    udpserver.Close();
+                    textBox3.AppendText("Closed data channel on UDP" + lport + "\r\n");
+                }
+                else if (command.StartsWith("exit"))
+                {
+                    button2_Click(this, new EventArgs());
+                }
+                else
+                {
+
+                    try
+                    {
+                        byte[] messageBytes = Encoding.ASCII.GetBytes(command + "\n");
+                        _ = client.Send(messageBytes);
+                        textBox3.AppendText("Sending: " + command + "\r\n");
+
+                        byte[] buffer = new byte[1024];
+                        int byterecv = client.Receive(buffer);
+                        var resp = Encoding.ASCII.GetString(buffer, 0, byterecv);
+                        textBox3.AppendText("Got response: " + resp + "\r\n");
+                    }
+                    catch (FormatException err)
+                    {
+                        button1.Enabled = true;
+                        textBox3.AppendText("Error: " + err.ToString());
+                    }
+                    catch (ArgumentNullException err)
+                    {
+                        button1.Enabled = true;
+                        textBox3.AppendText("Argument Null Exception: " + err.ToString());
+                    }
+                    catch (SocketException err)
+                    {
+                        button1.Enabled = true;
+                        textBox3.AppendText($"Socket Error: {err.Message}");
+                    }
+                }
+            }
+        }
+
+        private void textBox4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                button3_Click(this, new EventArgs());
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                button1_Click(this, new EventArgs());
+                e.Handled = true;
+            }
+        }
+    }
+}
